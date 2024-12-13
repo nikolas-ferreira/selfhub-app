e7d0962025c820aa42249cdb43a591e9f3cc44f1
nikolas <nikolas1903@icloud.com>
2024-12-13 09:02:27 -0300

    feat: putting landscape orientation

--- app/src/main/java/digital/studioweb/selfhub_app/MainActivity.kt
+++ app/src/main/java/digital/studioweb/selfhub_app/MainActivity.kt
@@ -1,20 +1,33 @@
 package digital.studioweb.selfhub_app
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import androidx.annotation.RequiresApi
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import dagger.hilt.android.AndroidEntryPoint
 import digital.studioweb.selfhub_app.ui.home.HomeScreen
-import kotlin.random.Random
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("WrongConstant")
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.let { controller ->
+            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         enableEdgeToEdge()
         setContent {
             HomeScreen()
